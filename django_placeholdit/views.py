85780f225ec896f8c0e4c4e6f088677afd26bd0a
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2025-09-27 17:09:16 +0300

    Django 5.1 fix `Internal Server Error: /^placeholders/160`

--- django_placeholdit/views.py
+++ django_placeholdit/views.py
@@ -4,17 +4,16 @@ from django.views.generic import View
 from wand.color import Color
 from wand.drawing import Drawing
 from wand.image import Image
-from django_placeholdit import settings
 
 
 class PlaceholderView(View):
-    def get(self, request, width, height, background, color, text, format):
+    def get(self, request, width, height=None, background=None, color=None, text=None, format=None):
         width = int(width)
         height = int(height or width)
         background = Color('#{}'.format(background or 'ccc'))
         color = Color('#{}'.format(color or '969696'))
         text = text or '{} x {}'.format(width, height)
-        format = format or 'gif'
+        fmt = format or 'gif'
 
         with Image(width=width, height=height, background=background) as image:
             with Drawing() as draw:
@@ -22,17 +21,12 @@ class PlaceholderView(View):
                 draw.font_size = width / 2
                 draw.text_alignment = 'center'
                 draw.text_encoding = 'UTF-8'
-                draw.font = settings.PLACEHOLDIT_FONT_PATH
-                
                 while True:
                     metrics = draw.get_font_metrics(image, text)
-
                     if metrics.text_width < width / 2:
                         break
-
                     draw.font_size -= 1
-
                 draw.text(int(width / 2), int(height / 2 + (metrics.text_height - metrics.ascender - metrics.descender) / 2), text)
                 draw(image)
 
-            return HttpResponse(image.make_blob(format), content_type='image/{}'.format(format))
+            return HttpResponse(image.make_blob(fmt), content_type='image/{}'.format(fmt))
